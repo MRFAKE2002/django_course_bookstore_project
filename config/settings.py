ce7e2a8f663ecf57316558953e2f83302985d98f
ROOZBEH <MR.FAKE@gmail.com>
2022-08-23 22:12:45 +0430

    add  static files and login and password_reset frontend

--- config/settings.py
+++ config/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     # third party apps
-    #'crispy_forms',
+    'crispy_forms',
     
     # my apps    
     'accounts',
@@ -124,7 +124,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
